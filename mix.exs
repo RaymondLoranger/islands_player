ed387d5303dc78c5f3151353280427d484c1f53d
RaymondLoranger <Raymond.Loranger@bell.net>
2020-03-06 14:10:01 -0500

    0.1.8

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Islands.Player.MixProject do
   def project do
     [
       app: :islands_player,
-      version: "0.1.7",
+      version: "0.1.8",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       name: "Islands Player",
@@ -48,7 +48,7 @@ defmodule Islands.Player.MixProject do
        github: "RaymondLoranger/mix_tasks", only: :dev, runtime: false},
       {:poison, "~> 3.0"},
       {:jason, "~> 1.0"},
-      {:persist_config, "~> 0.1"},
+      {:persist_config, "~> 0.2", runtime: false},
       {:islands_board, "~> 0.1"},
       {:islands_guesses, "~> 0.1"},
       {:earmark, "~> 1.0", only: :dev},
