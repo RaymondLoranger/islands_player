3fc0ae66f12d009807a84cbc4b7e5641654275dc
RaymondLoranger <Raymond.Loranger@bell.net>
2020-11-09 08:06:34 -0500

    0.1.18

--- lib/islands/player.ex
+++ lib/islands/player.ex
@@ -4,7 +4,8 @@
 defmodule Islands.Player do
   @moduledoc """
   Creates a `player` struct for the _Game of Islands_.
-  \n##### #{Islands.Config.get(:book_ref)}
+
+  ##### Based on the book [Functional Web Development](https://pragprog.com/book/lhelph/functional-web-development-with-elixir-otp-and-phoenix) by Lance Halvorsen.
   """
 
   alias __MODULE__
